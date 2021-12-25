9fb7b7fd5fa6f19195125475e8cc631c13b520df
Kartik <KartMahajan@gmail.com>
2021-12-24 21:14:06 -0800

    upate

--- website/src/Components/HomePage/TitleComponent.tsx
+++ website/src/Components/HomePage/TitleComponent.tsx
@@ -13,14 +13,14 @@ export class TitleComponent extends Component<TitleComponentProps>
     {
         return (
             <div className="title">
-                <div className="optionGrid">
-                    <p className="githubOption"><a href="https://github.com/KartikM123">github</a></p>
-                    <p className="linkedinOption"> <a href="https://www.linkedin.com/in/kartik-mahajan-36b885117/">linkedin</a> </p>
-                    <p className="randomOption"> <a href="https://www.youtube.com/watch?v=ZmrJUnkOZRE&ab_channel=kyle">random</a>  </p>
+                <div className="gridWrapper">
+                        <div className="githubOption"><a href="https://github.com/KartikM123">github</a></div>
+                        <div className="linkedinOption"> <a href="https://www.linkedin.com/in/kartik-mahajan-36b885117/">linkedin</a> </div>
+                        <div className="randomOption"> <a href="https://www.youtube.com/watch?v=ZmrJUnkOZRE&ab_channel=kyle">random</a>  </div> 
                 </div>
-                
+            
                 <div className="nameWrapper">
-                    <p className="fullName"> kartik mahajan</p><p className="fancyPeriod">.</p>
+                    <div className="fullName"> kartik mahajan</div><div className="fancyPeriod">.</div>
                 </div>
             </div>
         );
