833b6ef88ad1bce2bc7393991e21b6687a65cb70
Kartik <KartMahajan@gmail.com>
2021-12-24 20:22:08 -0800

    update styling for header... still have a bit more work to do

--- website/src/Components/HomePage/TitleComponent.tsx
+++ website/src/Components/HomePage/TitleComponent.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import '../StyleSheets/HomePage.css';
 interface TitleComponentProps
 {
 }
@@ -12,8 +12,16 @@ export class TitleComponent extends Component<TitleComponentProps>
     render()
     {
         return (
-            <div>
-                <h1> Kartik Mahajan </h1>
+            <div className="title">
+                <div className="optionGrid">
+                    <p className="githubOption"><a href="https://github.com/KartikM123">github</a></p>
+                    <p className="linkedinOption"> <a href="https://www.linkedin.com/in/kartik-mahajan-36b885117/">linkedin</a> </p>
+                    <p className="randomOption"> <a href="https://www.youtube.com/watch?v=ZmrJUnkOZRE&ab_channel=kyle">random</a>  </p>
+                </div>
+                
+                <div className="nameWrapper">
+                    <p className="fullName"> kartik mahajan</p><p className="fancyPeriod">.</p>
+                </div>
             </div>
         );
     }
