d4fd9f7dfe6eb4c362373bba37314e0b5cbbd32e
Kartik <KartMahajan@gmail.com>
2021-12-26 16:47:33 -0800

    easy

---
+++ website/src/Components/HomePage/PopUpTypes/ResearchPopUp.tsx
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import {default as ActivitiesJson} from "../../../Static/activities.json";
+import '../../StyleSheets/PopUp.css';
+import '../../StyleSheets/Tags.css';
+import logo from '../../../images/test/square1.svg';
+import logo2 from '../../../images/test/square2.svg';
+import logo3 from '../../../images/test/square3.svg';
+interface ResearchPopUpProps
+{
+    year: string;
+    eventName: string;
+    tagName: string;
+    toggle: () => void;
+}
+interface ResearchopUpState
+{
+    open: boolean
+}
+
+export class ResearchPopUp extends Component<ResearchPopUpProps, ResearchopUpState>
+{
+    constructor(props:ResearchPopUpProps)
+    {
+        super(props);
+        this.state = 
+        {
+            open: true
+        }
+    }
+
+    handleClick = () => {
+        this.props.toggle();
+    }
+
+    render()
+    {
+        return (
+            <div className="modal_info">
+                <p className="modal_description">Affiliated with: Interaction Lab</p>
+                <div className="modal_images">
+                    <img src={logo} className="modalImage1"/>
+                    <img src={logo2} className="modalImage2"/>
+                    <img src={logo3} className="modalImage3"/>     
+                </div>
+                <p className="modal_fulldescription"> 
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                </p>
+
+                <div className ="modal_links">
+                    <p className="readLink"><a href="https://localhost:3000"> read about it </a></p>\
+                    <p className="otherLink"><a href="https://localhost:3000"> learn about the work </a></p>
+                </div>
+            </div>
+        )
+    }
+}
